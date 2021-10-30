054a53b6eee0cfbc230beed8342aa5adf0991647
mehmetA58 <mehmet.akbayir58@gmail.com>
2021-10-30 13:44:20 +0300

    framework

---
+++ src/test/java/tests/lufiye/C01.java
@@ -0,0 +1,16 @@
+package tests.lufiye;
+
+import org.testng.annotations.Test;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class C01 {
+
+
+
+
+    @Test
+    public void test1(){
+        Driver.getDriver().get(ConfigReader.getProperty("Url"));
+    }
+}
